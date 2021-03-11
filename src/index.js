7e1ee428661fddc2d6acae64c54be007ab55a9a4
Artem Kolodka <73796530+Kola-art@users.noreply.github.com>
2021-03-12 01:26:19 +0200

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let eq = str.length;
+        let res = "";
+      for (let [x, y] of bracketsConfig) {
+        
+        str = str.split(`${x}${y}`).join('');
+      }
+
+      if (str.length === 0) {
+        res = true;
+        return res;
+      }
+      else if (str.length === eq) {
+        res = false;
+        return res;
+      }
+    
+      return check(str, bracketsConfig);
 }
+
